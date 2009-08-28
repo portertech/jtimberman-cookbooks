de4d0b82bee4641c725c33970e1485c548081f58
Joshua Timberman <jtimberman@virt1test.int.housepub.org>
2009-08-27 18:49:59 -0600

    string, not float

--- rsyslog/recipes/default.rb
+++ rsyslog/recipes/default.rb
@@ -49,7 +49,7 @@ end
 
 case node[:platform]
 when "ubuntu"
-  if node[:platform_version] >= 9.10
+  if node[:platform_version] >= "9.10"
     template "/etc/rsyslog.d/50-default.conf" do
       source "50-default.conf.erb"
       owner "root"
