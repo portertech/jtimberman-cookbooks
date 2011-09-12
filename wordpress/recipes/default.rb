2f6749610d856dfa2842c242f094809e36642dce
jtimberman <joshua@opscode.com>
2011-09-12 13:51:02 -0600

    mod_php5 recipe first so module list is generated

--- wordpress/recipes/default.rb
+++ wordpress/recipes/default.rb
@@ -18,10 +18,10 @@
 #
 
 include_recipe "apache2"
+include_recipe "apache2::mod_php5"
 include_recipe "mysql::server"
 include_recipe "php"
 include_recipe "php::module_mysql"
-include_recipe "apache2::mod_php5"
 
 if node.has_key?("ec2")
   server_fqdn = node['ec2']['public_hostname']
