01e083390f6c7b7d3eaab48e5ed6defaeba1e6ab
jtimberman <joshua@opscode.com>
2011-09-12 01:06:18 -0600

    COOK-744, use Gem.default_bindir for passenger_apache2

--- passenger_apache2/recipes/default.rb
+++ passenger_apache2/recipes/default.rb
@@ -47,6 +47,6 @@ gem_package "passenger" do
 end
 
 execute "passenger_module" do
-  command 'passenger-install-apache2-module --auto'
+  command "#{Gem.default_bindir}/passenger-install-apache2-module --auto"
   creates node[:passenger][:module_path]
 end
