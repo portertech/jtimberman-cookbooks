4c12513f7c168dbb485cf3235213304557e9d707
Sean Porter <portertech@gmail.com>
2012-01-19 08:25:41 -0800

    [rsyslog-lwrp] user, group, mode not in use, handled by rsyslog

--- rsyslog/resources/log.rb
+++ rsyslog/resources/log.rb
@@ -27,9 +27,6 @@ actions :create, :delete
 
 attribute :name,       :kind_of => String, :name_attribute => true
 attribute :path,       :kind_of => String, :required => true
-attribute :mode,       :regex => /^0?\d{3,4}$/, :default => 0755
-attribute :owner,      :regex => [ /^([a-z]|[A-Z]|[0-9]|_|-)+$/, /^\d+$/ ], :default => "root"
-attribute :group,      :regex => [ /^([a-z]|[A-Z]|[0-9]|_|-)+$/, /^\d+$/ ], :default => "root"
 attribute :max_size,   :kind_of => Integer, :default => 100
 attribute :rotations,  :kind_of => Integer, :default => 2
 attribute :programs,   :kind_of => Array
